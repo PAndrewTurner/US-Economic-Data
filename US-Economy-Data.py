06114a9f39fdf5454a3ff14ec5e8cc0734b2d415
P. Andrew Turner <Aturner326@gmail.com>
2025-12-01 00:47:15 -0500

    py update

--- US-Economy-Data.py
+++ US-Economy-Data.py
@@ -10,9 +10,8 @@ imports = monthly[['date', 'Imports', 'Exports', 'Imports % Change', 'Exports % 
 
 Unemployment = pd.read_excel("Unemployment Claims.xlsx")
 
-#Page Configuration
+# Page Configuration
 st.set_page_config(layout="wide")
-
 
 st.title("Trump Economy Tracker")
 
@@ -33,7 +32,7 @@ with col1:
     st.write("""
     Inflation is the change in prices over time. The inflation rate is calculated by comparing the prices of items now against the same time from the prior year.
     For example, the Inflation rate for October 2025 is calculated by seeing the change in prices from October 24th. to October 25th. Prices are calculated via the CPI or PPI metric.
-    
+
     The Consumer Price Index (CPI) and the Producer Price Index (PPI) are both key indicators of inflation, but they measure price changes from different perspectives in the economy. 
     The CPI focuses on the prices consumers pay, while the PPI tracks the prices producers receive for their goods and services at earlier stages of production.
     """)
@@ -44,9 +43,11 @@ with col1:
         st.metric(label="CPI Inflation Rate (as of " + inflation['date'].iloc[-1] + ")", value=most_recent, border=True)
 
     with metric_col_2:
-        st.metric(label="PPI Inflation Rate (as of " + inflation['date'].iloc[-1] + ")", value=most_recent2, border=True)
+        st.metric(label="PPI Inflation Rate (as of " + inflation['date'].iloc[-1] + ")", value=most_recent2,
+                  border=True)
 
-    st.line_chart(inflation, x='date', y=['CPI Inflation Rate', 'PPI Inflation Rate'], x_label='Date', y_label='Inflation Rate')
+    st.line_chart(inflation, x='date', y=['CPI Inflation Rate', 'PPI Inflation Rate'], x_label='Date',
+                  y_label='Inflation Rate')
 
 with col2:
     most_recent = imports['Imports % Change'].iloc[-2]
@@ -64,7 +65,7 @@ with col2:
     st.write("""
    The Import and Export Price Indexes indicators track the price changes of goods and services traded between the United States and other countries, excluding military items. 
    The Import Price Index measures the average change in the prices paid by U.S. residents for foreign products. Conversely, the Export Price Index tracks the average change in prices received by U.S. producers for products sold abroad. 
-   
+
    These indexes are typically presented as a year over year percent change. A positive Y/Y change indicates that prices for those traded goods are higher. A negative Y/Y change shows that prices have decreased. By monitoring this Y/Y metric, analysts can assess the recent momentum and underlying trend of inflation or deflation stemming from international commerce.
    """)
 
@@ -78,3 +79,21 @@ with col2:
                   border=True)
 
     st.line_chart(imports, x='date', y=['Imports % Change', 'Exports % Change'], x_label='Date', y_label='% Change')
+
+col3, col4 = st.columns(2)
+
+unemployment = monthly[['date', 'Unemployment Rate', 'U6 Unemployment Rate']]
+unemployment_by_race = monthly[
+    ['date', 'White Unemployment Rate', 'Black Unemployment Rate', 'Hispanic Unemployment Rate',
+     'Asian Unemployment Rate']]
+
+with col3:
+    st.header("Total Unemployment Rate\n", divider="gray")
+    st.line_chart(unemployment, x='date', y=['Unemployment Rate', 'U6 Unemployment Rate'], x_label='Date',
+                  y_label='Unemployment Rate')
+
+with col4:
+    st.header("Unemployment Rate by Race\n", divider="gray")
+    st.line_chart(unemployment_by_race, x='date',
+                  y=['White Unemployment Rate', 'Black Unemployment Rate', 'Hispanic Unemployment Rate',
+                     'Asian Unemployment Rate'], x_label='Date', y_label='Unemployment Rate')
