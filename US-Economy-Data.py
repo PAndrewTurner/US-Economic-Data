5cd2913fbdaf060df403a271396cbc13a03cc803
P. Andrew Turner <Aturner326@gmail.com>
2025-12-01 01:11:42 -0500

    Py uodate

--- US-Economy-Data.py
+++ US-Economy-Data.py
@@ -15,7 +15,7 @@ st.set_page_config(layout="wide")
 
 st.markdown("<h1 style='text-align: center;'>Trump Economy Tracker</h1>", unsafe_allow_html=True)
 
-st.write("")
+st.write("\n")
 
 col1, col2 = st.columns(2)
 
