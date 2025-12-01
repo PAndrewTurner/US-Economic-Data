c39b37d0094606aea0e6e970bd67bdc65a5ace3b
P. Andrew Turner <Aturner326@gmail.com>
2025-11-30 22:51:54 -0500

    main update

--- US-Economy-Data.py
+++ US-Economy-Data.py
@@ -1,7 +1,16 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import openai
+
+st.title("This is a title")
 
 st.write(
     """
     Hello, this is my app!   
     """
 )
+
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
